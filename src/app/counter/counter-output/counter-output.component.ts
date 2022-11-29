da10d6606b90de21bdff2a818afa582c881ce253
Abdul Kyume <abbirtak3@gmail.com>
2022-11-29 18:05:55 +0600

    day-1

---
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.css']
+})
+export class CounterOutputComponent implements OnInit {
+  // @Input() counter: any;
+  counter!:number;
+  constructor(private store: Store<{counter:{counter:number}}>) { }
+
+  ngOnInit(): void {
+    this.store.select('counter').subscribe((data:any)=>{
+      this.counter = data.counter;
+    })
+  }
+
+}
---
+++ src/app/counter/state/counter.reducer.ts
@@ -0,0 +1,17 @@
+import { decrement, increment, reset } from './counter.action';
+import { createReducer,on } from "@ngrx/store";
+import { initialState } from './counter.state'
+
+const _counterReducer = createReducer(initialState, on(increment, (state)=>{
+    return {...state, counter:state.counter+1};
+}),
+on(decrement, (state:any)=>{
+    return {...state, counter:state.counter-1};
+}),
+on(reset, (state:any)=>{
+    return {...state, counter:0};
+})
+);
+export function counterReducer(state:any,action:any){
+    return _counterReducer(state,action);
+}
